c87719209fd987671b55b5795347392a1432285f
Anti Thesis <antithesisx@yandex.com>
2014-01-26 21:27:08 +0100

    Updated installer.

--- install.sh
+++ install.sh
@@ -12,8 +12,32 @@ cp keepwritings recall /usr/local/bin
 # Create the directories necessary for the scripts to operate.
 if [[ ! -d ~/.config/keepwritings ]]
 	then mkdir -p ~/.config/keepwritings/history
-	touch ~/.config/keepwritings/current.txt
 fi
 
-# Install the dependencies.
-apt-get install -y focuswriter suckless-tools xclip xautomation
+
+# Install scribe and revcontrol from the github
+git clone https://github.com/Antithesisx/scribe.git
+cd scribe
+sudo cp scribe /usr/bin/
+sudo chmod +x /usr/bin/scribe
+cd ..
+git clone https://github.com/Antithesisx/Revcontrol.git
+cd Revcontrol
+sudo cp revcontrol /usr/bin/
+sudo chmod +x /usr/bin/revcontrol
+
+# Install the other dependencies. For manual installation: just use your package manager to install xclip, dmenu, inotify-tools and xautomation.
+error() {
+echo "$(basename "$0"):" "error:" "$@" 1>&2
+exit 1
+}
+
+depends() {
+local d
+for d
+do
+command -v "$d" >/dev/null || error "Missing dependency:" "$d"
+done
+}
+
+depends xclip dmenu xautomation inotify-tools
